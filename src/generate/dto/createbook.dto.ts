6a62c694e222411a45ef3fc5f2f389ebb27f9429
Theo Kolb <s0579788@htw-berlin.de>
2023-05-11 13:33:14 +0200

    merge with main

--- src/generate/dto/createbook.dto.ts
+++ src/generate/dto/createbook.dto.ts
@@ -1,8 +1,6 @@
 import { IsNotEmpty } from 'class-validator';
-import { ChildInfo } from "../interfaces/childinfo.interface";
-import { TopicInfo } from "../interfaces/topicinfo.interface";
 
 export class CreateBookDto {
-  @IsNotEmpty() child: ChildInfo;
-  @IsNotEmpty() topic: TopicInfo;
+  //@IsNotEmpty() child: ChildInfo;
+  //@IsNotEmpty() topic: TopicInfo;
 }
