5b2d277577cabf474c65bec904f63e2ae3340717
sotenck <z8F-2fX;~#8>
2023-05-12 11:14:38 +0200

    added variables to http-test files added delete-api-key functionality

--- src/administration/administration.controller.ts
+++ src/administration/administration.controller.ts
@@ -1,18 +1,27 @@
-import { Controller, Get, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
 import { AdministrationService } from "./administration.service";
 import { AuthGuard } from "@nestjs/passport";
 import { UserTypeGuard } from "../authentification/roles/type.guard";
+import { ApiKeyHashDto } from "./dto/api-key-hash.dto";
+import { ApiKeyInterface } from "./interface/api-key.interface";
 
+@UseGuards(
+  AuthGuard('jwt'),
+  UserTypeGuard('admin')
+)
 @Controller('admin')
 export class AdministrationController {
   constructor(private readonly adminService: AdministrationService ) {}
 
-  @UseGuards(
-    AuthGuard('jwt'),
-    UserTypeGuard('admin')
-  )
   @Get('create-key')
-  async createKey(): Promise<any> {
+  async createKey(): Promise<ApiKeyInterface> {
     return await this.adminService.createKey();
   }
+
+  @Post('remove-key')
+  async removeKey(@Body() apiKeyHashDto: ApiKeyHashDto): Promise<any> {
+    return await this.adminService.removeKey(apiKeyHashDto);
+  }
+
+
 }
--- src/administration/dto/api-key-hash.dto.ts
+++ src/administration/dto/api-key-hash.dto.ts
@@ -1,5 +1,5 @@
 import { IsNotEmpty } from 'class-validator';
 
-export class ApiKeyDto {
-  @IsNotEmpty() apiKey: string;
+export class ApiKeyHashDto {
+  @IsNotEmpty() apiHash: string;
 }
