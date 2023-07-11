ca7f9b2a7b7fbeabcea0f89f78766ccf7074ab09
Marco Thies <s0580114@htw-berlin.de>
2023-07-11 19:43:19 +0200

    adjusted openai prompting generator

--- src/_subservices/text-prompt-designer.subservice.ts
+++ src/_subservices/text-prompt-designer.subservice.ts
@@ -9,59 +9,72 @@ export class TextPromptDesignerSubservice {
   public generateStoryPrompt(parameter: ParameterEntity): IOpenAiPromptMessage[] {
     console.log(parameter);
 
-    const moral = (parameter.optMoral) ? parameter.optMoral : 'default moral';
-
     // generate Text-Prompt from Child Parameters
     const paragraphCount = parameter.optChapterCount;
     const avgWordsPerParagraph = 60;
     let author : string = 'Lewis Carroll';
+    let moral : string = "Freundschaft";
 
     const randomNumber = Math.floor(Math.random() * 5);
     switch(randomNumber) {
       case 0:
         author = 'Astrid Lindgren';
+        moral = "Freundschaft ist sehr wichtig";
         break;
       case 1:
         author = 'Eric Carle';
+        moral = "Anderen zu helfen ist erfüllend";
         break;
       case 2:
         author = 'Dr. Seuss';
+        moral = "Ehrlichkeit währt am längsten";
         break;
       case 3:
         author = 'Roald Dahl';
+        moral = "Jeder ist seiens eigenen Glückes Schmied";
         break;
       case 4:
         author = 'Lewis Carroll';
+        moral = "Es ist noch kein Meister vom Himmel gefallen";
         break;
     }
-
 
     const systemContent =
       "Antworte, als wärst du Kinderbuchautor/in " + author + ".\n" +
 
-      "Bitte stelle sicher, dass die Geschichte altersgerecht und ansprechend ist, mit einer klaren Erzählung, die die gewählte Moral und das Thema beinhaltet. \n" +
-      "Die Geschichte sollte auch Elemente enthalten, die die Interessen und Persönlichkeitsmerkmale meines Kindes widerspiegeln. \n" +
+      "Bitte stelle sicher, dass die Geschichte kindergerecht und ansprechend ist, mit einer klaren Erzählung, die die gewählte Moral und das Thema beinhaltet. \n" +
+      "Die Geschichte sollte auch Elemente enthalten, die die gefragten Details widerspiegeln. \n" +
       "Erwähne sie aber nicht explizit, sondern richte die Geschichte geschickt an ihnen aus. Erwähne die Moral nicht öfter als ein mal. \n" +
       "Die Geschichte soll auch einen Dialog enthalten und einem roten Faden folgen."+
       "\n\n" +
-      "Eine Geschichte besteht immer aus einem Titel und mehreren paragraphen Text.\n" +
+      "Eine Geschichte besteht immer aus einem Titel und mehreren Paragraphen Text.\n" +
       "Außerdem gehört zu jeder Geschichte eine Liste mit auftretenden Characteren jeweils mit einer bildlichen Beschreibung.\n" +
       "Die Character-Beschreibung sollte sehr genau auf das Aussehen, Kleidung und Außenwirkung des jeweiligen Characters eingehen.";
 
-      let userContent =
-      "Generiere mir ein personalisiertes Kinderbuch, das auf die Interessen und Persönlichkeit meines Kindes zugeschnitten ist. \n" +
-      "Das Buch muss " + paragraphCount + " Paragraphen mit je mindestens " + avgWordsPerParagraph + " Wörtern haben (mindestens zwei volle Sätze). \n" +
-      "Hier sind Details über mein Kind, die du auf die Geschichte anwenden kannst:\n\n" +
+    let userContent =
+    "Generiere mir ein personalisiertes Kinderbuch, das auf die Interessen und Persönlichkeit meines Kindes zugeschnitten ist. \n" +
+    "Das Buch muss " + paragraphCount + " Paragraphen mit je mindestens " + avgWordsPerParagraph + " Wörtern haben (mindestens zwei volle Sätze). \n" +
+    "Hier sind Details über den Hauptcharakter des Buchs und weitere Details, die in der Geschichte vorkommen sollen:\n\n" +
 
-      "- Name: " + parameter.charName + "\n" +
-      "- Alter: " + parameter.charAge + "\n" +
-      "- Geschlecht: " + parameter.charGender + "\n";
+    "Daten zum Hauptcharakter: \n" +
+    "- Name: " + parameter.charName + "\n" +
+    "- Alter: " + parameter.charAge + "\n" +
+    "- Geschlecht: " + parameter.charGender + "\n";
 
-      if(parameter.optTopic) userContent += "- Lieblingsthema: " + parameter.optTopic + "\n";
+    if(parameter.optColor) userContent += "- Lieblingsfarbe: " + parameter.optColor + "\n\n";
 
-      userContent += ""+
-      "Erwähne die Lieblingsfarbe und das Lieblingstier/-charakter im ersten Absatz nicht! \n" +
-      "Achte auf eine ausreichende Länge der Absätze. (Wie gesagt, mindestens "+ avgWordsPerParagraph +" Wörter pro Paragraph)";
+    userContent += "Daten zur Geschichte: \n";
+
+    if(parameter.optTopic) userContent += "- Thema der Geschichte: " + parameter.optTopic + "\n";
+    if(parameter.optSidekick) userContent += "- Nebencharakter der Geschichte: " + parameter.optSidekick + "\n";
+    if(parameter.optPlace) userContent += "- Schauplatz: " + parameter.optPlace + "\n";
+
+    if(parameter.optMoral) userContent += "- Moral der Geschichte: " + parameter.optMoral + "\n";
+    else userContent += "- Moral der Geschichte: " + moral + "\n";
+
+    userContent += "\n"+
+    "Erwähne die Lieblingsfarbe nicht im ersten Absatz! \n" +
+    "Achte auf eine ausreichende Länge der Absätze. (Wie gesagt, mindestens "+ avgWordsPerParagraph +" Wörter pro Paragraph)";
 
     return [
         { role : messageRole.system , content : systemContent },
